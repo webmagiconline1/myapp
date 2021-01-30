01f02c953fd13a51724f57f500434c3525252d7a
Dabeer Shaikh <dab8106@gmail.com>
2021-01-30 15:58:27 +0530

    Development Code

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World from WebMagic !!!!!');
+  res.end('Hello World from WebMagic Development');
 });
 
 server.listen(port, hostname, () => {
